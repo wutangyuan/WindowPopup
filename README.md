e70ccfd74a501779103371224f16983b8fc1c3e7
wutangyuan <wutangyuan1@gmail.com>
2021-04-18 11:30:08 +0800

    firstcommit

---
+++ README.md
@@ -0,0 +1,10 @@
+# WindowPopup
+<<<<<<< HEAD
+窗体叠加，解决WPF调用winform或者GDI进程的空域问题
+=======
+<<<<<<< HEAD
+窗体叠加，解决WPF调用winform或者GDI进程的空域问题
+=======
+WPf窗体叠加，解决空域的问题
+>>>>>>> ffdacd2... add File
+>>>>>>> 3305b8c... add File
---
+++ WpfTo3D/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTo3D
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WpfUPWindow WpfUPWindow = new WpfUPWindow();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //WpfUPWindow.Topmost = true;
+            //this.Topmost = true;
+            WpfUPWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            WindowMove = WpfUPWindow.ParentMoveAction;
+            WpfUPWindow.Show();
+        }
+
+        private Action<double, double> WindowMove;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WpfUPWindow.Width = this.ActualWidth;
+            WpfUPWindow.Height = this.ActualHeight;
+            WpfUPWindow.Top = this.Top;
+            WpfUPWindow.Left = this.Left;
+            WpfUPWindow.Owner = this;   
+        }
+
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+            WindowMove?.Invoke(this.Left, this.Top);
+        }
+    }
+}
